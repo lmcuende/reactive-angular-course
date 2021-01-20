821c29caa5f8a6f41ab7589d1697775c48255446
lmcuende <lmcuende@gmail.com>
2021-01-21 00:45:36 +0100

    feat: Courses Store

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -7,13 +7,15 @@ import {catchError} from 'rxjs/operators';
 import {throwError} from 'rxjs';
 import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../loading/loading.service';
+import { MessagesService } from '../messages/messages.service';
 
 @Component({
     selector: 'course-dialog',
     templateUrl: './course-dialog.component.html',
     styleUrls: ['./course-dialog.component.css'],
     providers: [
-        LoadingService
+        LoadingService,
+        MessagesService
     ]
 })
 export class CourseDialogComponent  {
@@ -27,7 +29,8 @@ export class CourseDialogComponent  {
         private dialogRef: MatDialogRef<CourseDialogComponent>,
         @Inject(MAT_DIALOG_DATA) course:Course,
         private coursesService: CoursesService,
-        private loadingService: LoadingService) {
+        private loadingService: LoadingService,
+        private messagesService: MessagesService) {
 
         this.course = course;
 
@@ -43,7 +46,15 @@ export class CourseDialogComponent  {
 
       const changes = this.form.value;
 
-      const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes);
+      const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes)
+        .pipe(
+            catchError(err => {
+                const message = "Could not save course";
+                console.log(message, err);
+                this.messagesService.showErrors(message);
+                return throwError(err);
+;            })
+        );
       this.loadingService.showLoaderUntilCompleted(saveCourse$)
         .subscribe(
             val => {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../loading/loading.service';
 import { MessagesService } from '../messages/messages.service';
+import { CoursesStore } from '../services/courses.store';
 
 
 @Component({
@@ -22,9 +23,7 @@ export class HomeComponent implements OnInit {
 
 
   constructor(
-    private coursesService: CoursesService,
-    private loadingService: LoadingService,
-    private messagesService: MessagesService) {
+    private coursesStore: CoursesStore) {
 
   }
 
@@ -34,34 +33,7 @@ export class HomeComponent implements OnInit {
 
   reloadCourses() {
 
-    const courses$ = this.coursesService.loadAllCourses()
-      .pipe(
-        map(courses => courses.sort(sortCoursesBySeqNo)),
-        catchError(err => {
-          const message = "Could not load courses";
-          this.messagesService.showErrors(message);
-          console.log(message, err);
-          return throwError(err);
-        })
-      );
-
-    const loadCourses$ = this.loadingService.showLoaderUntilCompleted(courses$);
-
-    this.beginnerCourses$ = loadCourses$
-      .pipe(
-        map(courses => courses.filter(course => course.category == "BEGINNER"))
-      );
+    this.beginnerCourses$ = this.coursesStore.filterByCategory("BEGINNER");
       
-    this.advancedCourses$ = loadCourses$
-      .pipe(
-        map(courses => courses.filter(course => course.category == "ADVANCED"))
-      );
-  }
-
-  
-
+    this.advancedCourses$ = this.coursesStore.filterByCategory("ADVANCED");
 }
-
-
-
-
