0155169c800d3cfd03dbbd32878ec5ea706e954c
lmcuende <lmcuende@gmail.com>
2021-01-24 20:13:28 +0100

    feat: mem authstore localstorage

--- src/app/services/auth.store.ts
+++ src/app/services/auth.store.ts
@@ -1,12 +1,12 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable } from "rxjs";
-import { map, shareReplay } from "rxjs/operators";
+import { map, shareReplay, tap } from "rxjs/operators";
 import { User } from "../model/user";
 
-
+const AUTH_DATA = "auth_data";
 @Injectable({
-    providedIn: 'root;'
+    providedIn: 'root'
 })
 export class AuthStore {
 
@@ -22,13 +22,22 @@ export class AuthStore {
         this.isLoggedIn$ = this.user$.pipe(map(user => !!user));
 
         this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn));
+
+        const user = localStorage.getItem(AUTH_DATA);
+
+        if (user) {
+            this.subject.next(JSON.parse(user));
+        }
     }
 
     login(email: string, password: string): Observable<User> {
 
         return this.http.post<User>("/api/login", {email, password})
             .pipe(
-                tap(user => this.subject.next(user)),
+                tap(user => {
+                    this.subject.next(user);
+                    localStorage.setItem(AUTH_DATA, JSON.stringify(user));
+                }),
                 shareReplay()
             );
 
@@ -36,6 +45,7 @@ export class AuthStore {
 
     logout() {
         this.subject.next(null);
+        localStorage.removeItem(AUTH_DATA);
 
     }
 
