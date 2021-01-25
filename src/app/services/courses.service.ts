a73ac89426dbf747940df0e45bb88ce9599970c5
lmcuende <lmcuende@gmail.com>
2021-01-26 00:59:17 +0100

    feat: search

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Course } from '../model/course';
+import { Lesson } from '../model/lesson';
 import { map, shareReplay } from 'rxjs/operators';
 
 @Injectable({
@@ -23,6 +24,19 @@ export class CoursesService {
     saveCourse(courseId: string, changes:Partial<Course>):Observable<any> {
         return this.http.put(`/api/courses/${courseId}`, changes)
             .pipe(
+                shareReplay()
+            );
+    }
+
+    searchLessons(search:string): Observable<Lesson[]> {
+        return this.http.get<Lesson[]>('/api/lessons', {
+            params: {
+                filter: search,
+                pageSize: "100"
+            }
+        })
+            .pipe(
+                map(res => res["payload"]),
                 shareReplay()
             );
     }
