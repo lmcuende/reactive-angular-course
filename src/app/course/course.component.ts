667c0007a7c5c43ac0bd1d0abaa7fdebeb401e11
lmcuende <lmcuende@gmail.com>
2021-01-28 01:00:39 +0100

    The single data Observable Pattern

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -13,10 +13,15 @@ import {
   withLatestFrom,
   concatAll, shareReplay, catchError
 } from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat, throwError} from 'rxjs';
+import {merge, fromEvent, Observable, concat, throwError, combineLatest} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { CoursesService } from '../services/courses.service';
+import { CoursesStore } from '../services/courses.store';
 
+interface CourseData {
+  course: Course;
+  lessons: Lesson[];
+}
 
 @Component({
   selector: 'course',
@@ -25,9 +30,7 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CourseComponent implements OnInit {
 
-  course$: Observable<Course>;
-
-  lessons$: Observable<Lesson[]>;
+  data$: Observable<CourseData>
 
   constructor(
     private route: ActivatedRoute,
@@ -40,9 +43,20 @@ export class CourseComponent implements OnInit {
 
     const courseId = parseInt(this.route.snapshot.paramMap.get("courseId"));
 
-    this.course$ = this.coursesService.loadCourseById(courseId);
+    const course$ = this.coursesService.loadCourseById(courseId);
 
-    this.lessons$ = this.coursesService.loadAllCourseLessons(courseId);
+    const lessons$ = this.coursesService.loadAllCourseLessons(courseId);
+
+    this.data$ = combineLatest([course$, lessons$])
+      .pipe(
+        map(([course, lessons]) => {
+          return {
+              course,
+              lessons
+          }
+        }),
+        tap(console.log)
+      );
 
 
   }
